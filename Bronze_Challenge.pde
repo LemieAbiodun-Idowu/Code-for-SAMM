3a83d6393d3b4d8cd4290c3d83a290df18a0d621
LemieAbiodun-Idowu <lemielovesben10@gmail.com>
2025-02-23 15:09:36 +0000

    i did it you bozo
    you succ lmao

--- Bronze_Challenge.pde
+++ Bronze_Challenge.pde
@@ -6,32 +6,32 @@ String distance = "0";
 String speed = "0";
 String warning = "";
 ControlP5 cp5;
-long prevUpdate = 0;
+long prevUpdate = 0;  
 
 void setup() {
   size(900, 900);
   background(50);
-  myClient = new Client(this, "192.168.4.1", 5200);
+  myClient = new Client(this, "192.168.4.1", 5200);  //,atches 5200 from arduino code
   cp5 = new ControlP5(this);
-  PFont buttonFont = createFont("Arial", 38);  // Create a font with size 32
-  cp5.addButton("GOButton")
+  PFont buttonFont = createFont("Arial", 38);  // Create a font with size 38
+  cp5.addButton("GOButton")  //go button
      .setLabel("GO")
-     //.getCaptionLabel().setSize(69.420)
+     //.getCaptionLabel().setSize(420)
      .setPosition(300,450)
      .setSize(200, 80)
-     .setColorBackground(color(0, 255, 0))  // Set background color to green
-     .setColorActive(color(255, 255, 255))
+     .setColorBackground(color(0, 255, 0))  // Set color to green
+     .setColorActive(color(255, 255, 255))  
      .setFont(buttonFont)  // Set the button label font
-     .onClick(new CallbackListener() {
+     .onClick(new CallbackListener() {  //use event handlers for when buttons are pressed
        public void controlEvent(CallbackEvent theEvent) {
          GObuttonClicked();
        }
      });
-  cp5.addButton("STOPButton")
+  cp5.addButton("STOPButton")  //stop button
      .setLabel("STOP")
      .setPosition(600, 450)
      .setSize(200, 80)
-     .setColorBackground(color(255, 0, 0))
+     .setColorBackground(color(255, 0, 0))  //Set color to red
      .setColorActive(color(255, 255, 255))
      .setFont(buttonFont)
      .onClick(new CallbackListener() {
@@ -44,23 +44,23 @@ void setup() {
 void draw() {
   background(50);
   textSize(50);
-  if (millis() - prevUpdate > 100){
+  if (millis() - prevUpdate > 100){    //Making sure values are updated regularly
     if(myClient.active()){
       myClient.write("Update lil bro\n");
       prevUpdate = millis();
     }
   }
-  while (myClient.available() > 0) {
+  while (myClient.available() > 0) {  //Remember .available means there is data waiting to be read
     String data = myClient.readStringUntil('\n');  // Read incoming data
      if(data != null){
-      data = data.trim();
+      data = data.trim();    //Removes whitespaces
       if(data.equals("Bout to hit something")){
         warning = "OBSTACLE!!!";
       }
       else if(data.equals("obstacle_cleared")){
         warning = "Cleared :D";
       }
-      else{
+      else{  //if the data sent is not warning about an obstacle it will be sending distance and speed
         String[] DST =  data.split(",");  // Split speed and distance
         if (DST.length == 2) {
           speed = DST[0];
@@ -69,17 +69,17 @@ void draw() {
       }
   }
  }
-  float speedVal = float(speed);
+  float speedVal = float(speed); 
   if (speedVal > 0) {
     fill(0, 255, 0); // Green when moving
   } else {
     fill(255, 0, 0); // Red when stopped
   }
   text("Speed: " + speed + "m/s", 1, 600);
-  fill(255);
+  fill(255);  //changes colour of the text to white
   text("Distance: " + distance + "m", 550, 600);
   
-  if (!warning.isEmpty()) {
+  if (!warning.isEmpty()) {    //if there is a warning
   if(warning.equals("OBSTACLE!!!")) {
         fill(255, 0, 0);  // Red for obstacle warning
     } else if(warning.equals("Cleared :D")) {
@@ -91,6 +91,7 @@ void draw() {
   }
 }
 
+//Event handlers
 void GObuttonClicked() {
   myClient.write("Proceed lil bro\n");
 
---
+++ Bronze_Challenge/Bronze_Challenge.ino
@@ -0,0 +1,268 @@
+#include <NewPing.h>    //used for US Sensor
+#include <WiFiS3.h>     //Library for wifi
+
+char ssid[] = "Y11 SAMM"; 
+char pass[] = "12345678"; 
+//our arduino is a Wireless Acess Point, it basically acts as its own wifi
+WiFiServer server(5200);  //the port number used
+WiFiClient client;
+
+//For wheel encoders
+//volatile ensures the values are the same when accessed outside the isr functions
+//use unsigned long because the values can get very big
+volatile unsigned long Time_ENC_A = 0;
+volatile unsigned long Time_ENC_B = 0;
+const float pi = 3.14159265;
+volatile unsigned long pulseCount_ENC_A = 0;
+volatile unsigned long pulseCount_ENC_B = 0;
+volatile unsigned long previous_time_A = 0;
+volatile unsigned long previous_time_B = 0;
+//the wheels have 4 pulses per revolution
+const int Pulse_pre_rev = 4;
+//wheel circumference
+const float Circumference = 6.2*pi/100.0; // divided by 100 for metres conversion .0 is used for float division
+
+//const values are NEVER changed
+const int LEYE = 4;   //Eye = IR Sensors
+const int REYE = 12;
+const int speed = 120;
+const int US_TRIG = 9;  //Sends the Ultrasonic Pulse
+const int US_ECHO = 8;  //Receives the Ultrasonic Pulse
+
+bool obstacle_detected = false;
+bool BuggyActive = false;
+
+//Motor A pins
+const int ENA = 5;    //EN = enable pins for the motor
+const int IN1 = 16;
+const int IN2 = 15;
+
+//Motor B pins
+const int ENB = 11;
+const int IN3 = 18;
+const int IN4 = 17;
+
+//Encoder pins, the wheel encoders help determine the distance and speed travelled
+//encoder pins have to be 2 and 3 because fucking arduino.
+const int ENC_A = 2;
+const int ENC_B = 3;
+
+int LEYE_current_state;
+int REYE_current_state;
+
+//unsigned long starting;
+//unsigned long ending;     debugging stuff
+int distance_maxxing = 50;  //maximum distance at which the us sensor detects objects
+
+//NewPing library helps remove unnecessary delays for US Sensor
+NewPing UltraSonic(US_TRIG, US_ECHO, distance_maxxing);
+
+float obst_distance = UltraSonic.ping_cm(); //current distance in cm of obstacle from US Sensor
+//volatile float distance_travelled = 0.0;  //this is distance travelled by buggy
+float avg_time = 0.0;     //this is the time taken between each pulse emitted by the wheels
+float avg_speed = 0.0;  //this is the speed between each pulse emitted by the wheels
+float avg_distance_travelled = 0.0; //the total distance travelle by buggy
+
+void setup() {
+  Serial.begin(115200); //115200 is the baud rate, this is the number of times the signal (messages) are sent per second
+  //increased from 9600 because some information wasnt being sent quick enough to the arduino
+  WiFi.beginAP(ssid, pass);   //used beginAP because its a WAP
+  Serial.print("Connecting to WiFi...");
+  while(WiFi.status() != WL_AP_CONNECTED){  //while wifi is not connected
+    Serial.print(".");
+    delay(1500);
+  }
+  Serial.println("Connected!");
+  Serial.print("IP Address:");   
+  Serial.println(WiFi.localIP());  //the IP Address
+  server.begin();
+
+  //Outputs give out info, Inputs receive Info
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(LEYE, INPUT);
+  pinMode(REYE, INPUT);
+  pinMode(US_TRIG, OUTPUT);
+  pinMode(US_ECHO, INPUT);
+  //Without INPUT_PULLUP → The encoder pin can randomly switch between HIGH and LOW, causing glitches.
+  pinMode(ENC_A, INPUT_PULLUP);
+  pinMode(ENC_B, INPUT_PULLUP);
+  //interrupts pause the main program and then run this function, which is time_monitor
+  //RISING means the function is run when the encoder pins (which are interrupted) go from LOW to HIGH
+  attachInterrupt(digitalPinToInterrupt(ENC_A), time_monitor_A, RISING);
+  attachInterrupt(digitalPinToInterrupt(ENC_B), time_monitor_B, RISING);
+}
+
+void loop() {
+  //starting = micros();
+  client = server.available();
+  if (client.connected()){  //used instead of (client) because this checks for an inactive connection
+    Serial.println("Client is still Online");
+    if(client.available()) {  //if there is data to be read
+      String RequestFromClient = client.readStringUntil('\n');  // Read client request
+      if (RequestFromClient == "Proceed lil bro")
+        Proceed();
+      else if (RequestFromClient == "Halt lil bro") 
+        Halt();
+      else if (RequestFromClient == "Update lil bro"){  //this is mainly used to just ensure new speed and distance values are sent
+      }
+    }
+ //   Serial.println("AVG Speed: " + String(avg_speed) + "," + "AVG Distance: " + String(avg_distance_travelled) + "\n");
+  }
+  Calc_avg_DST();
+  if (client.connected()){
+    client.print(String(avg_speed, 2) + "," + String(avg_distance_travelled, 2) + "\n");
+}
+  if(BuggyActive){
+    ActivateBuggy();  //this runs the ir sensor and us sensor stuff
+  }
+}
+
+//functions for moving help with code redability
+void moveForward(){
+  //Serial.println("moving forward");
+  analogWrite(ENA, speed);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN1, HIGH);
+  analogWrite(ENB, speed);
+  digitalWrite(IN4, HIGH);
+  digitalWrite(IN3, LOW);
+}
+
+void turnLeft(){
+  //Serial.println("turning left");
+  analogWrite(ENA, 0);  //one wheel turns off when turning
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN1, HIGH);
+  analogWrite(ENB, speed+35);
+  digitalWrite(IN4, HIGH);
+  digitalWrite(IN3, LOW); 
+}
+
+void turnRight(){
+  //Serial.println("turning right");
+  analogWrite(ENB, 0);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(ENA, speed+35);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN1, HIGH);
+}
+
+void stop(){
+  //Serial.println("stopping");
+  analogWrite(ENA, 0);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN1, LOW);
+  analogWrite(ENB, 0);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+
+//isr functions
+void time_monitor_A(){
+  unsigned long current_time = micros();
+  if(previous_time_A > 0){
+    Time_ENC_A = current_time - previous_time_A;
+  }
+  previous_time_A = current_time;
+  pulseCount_ENC_A++;
+ 
+}
+
+void time_monitor_B(){
+  unsigned long current_time = micros(); 
+  if(previous_time_B > 0){
+    Time_ENC_B = current_time - previous_time_B;
+  }
+  previous_time_B = current_time;
+  pulseCount_ENC_B++;
+}
+
+
+void Calc_avg_DST() {
+  unsigned long total_pulses = pulseCount_ENC_A + pulseCount_ENC_B;
+  // Serial.print("Pulse Count a: ");
+  // Serial.println(pulseCount_ENC_A);
+  // Serial.print("Pulse Count b: ");
+  // Serial.println(pulseCount_ENC_B);
+  // Serial.print("Total pulses: ");
+  // Serial.println(total_pulses);
+
+  //you travel once circumference per revolution, the *2 is because of both wheels
+  avg_distance_travelled = Circumference*total_pulses/((float)(Pulse_pre_rev * 2));
+  
+  // Calculate time in seconds
+  avg_time = (Time_ENC_A + Time_ENC_B)/2000000.0; // because Time_ENC_A and B are measured in mircoseconds
+  if(avg_time > 0.0001) { // helps avoid division by very small numbers
+    avg_speed = Circumference/(Pulse_pre_rev*avg_time); //remember this is the speed for each pulse
+  } else {
+      avg_speed = 0;  //if time between pulses is just say speed is 0 at that point
+  }
+        
+  
+}
+
+void obstacle_detection(float distance){
+  if(distance > 0){ //ignore if the sensor says 0 or if it somehow says < 0
+    if(distance < 15 && !obstacle_detected){ //if an obstacle is close and it hasnt reported an obstacle yet
+      Serial.println("Holy Crap I'm about to hit something");
+      client.print("Bout to hit something\n");
+      obstacle_detected = true; //now it knows the message has been sent that the obstacle has been detected
+    }
+    else if(distance > 15 && obstacle_detected){
+      obstacle_detected = false;  //obstacle is now no longer there
+      client.print("obstacle_cleared\n"); //client.print sends stuff to the client, which is processing
+    }
+  }
+  else {
+    obstacle_detected = false;
+    client.print("obstacle_cleared\n");
+  }
+}
+
+void Proceed(){
+  BuggyActive = true; //now knows that the buggy is available to be moved
+}
+
+void Halt(){
+  stop(); 
+  BuggyActive = false;  //now knows that the buggy shouldnt be moved at all
+  avg_speed = 0;  //speed and time between pulses are reset to 0 when halted
+  Time_ENC_A = 0;
+  Time_ENC_B = 0;
+}
+
+void ActivateBuggy(){
+  LEYE_current_state = digitalRead(LEYE);
+  REYE_current_state = digitalRead(REYE); //re-read the ir sensor state each loop
+
+  obst_distance = UltraSonic.ping_cm(); //current distance in cm from US Sensor
+  // if(obst_distance > 0 ){
+  //   Serial.print("Distance: ");
+  //   Serial.println(obst_distance); 
+  //  }
+
+  obstacle_detection(obst_distance);
+  if(obstacle_detected){
+    stop();
+  }
+  else if(LEYE_current_state && REYE_current_state){ 
+    moveForward();
+  }
+  else if(!LEYE_current_state && REYE_current_state){ //!LEYE_current_state basically means if LEFT IR Sensor is off
+    turnLeft();
+  }
+  else if(!REYE_current_state && LEYE_current_state){
+    turnRight();
+  }
+  else if (!LEYE_current_state && !REYE_current_state){
+    stop();  
+  }
+  //ending = micros();
+  //Serial.println("loop " + String(ending - starting));
+}
